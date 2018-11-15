6495d8ac33321ca7ee8a768507880a50841ac1bc
bryan boconzaca <bboconzaca2022@bklawtech.com>
2018-11-15 23:38:01 +0000

    added random number

--- js/script.js
+++ js/script.js
@@ -4,8 +4,27 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
+var userChoice =""
+var computerChoice =""
+var winner =""
+var randomNumber = 0;
 
 // DOCUMENT READY FUNCTION BELOW
+$("button").click(function() {
+ userChoice = $("#input").val();
+$("#userChoice").text(userChoice);
+    randomNumber= Math.random();
+    if(randomNumber<0.333){
+    computerChoice="rock";    
+    }
+    else if(randomNumber<0.666){
+        computerChoice="paper";
+    }
+    else{
+        computerChoice="scissors";
+        }
+
+$("#computerChoice").text(computerChoice);
+    
+});
 
